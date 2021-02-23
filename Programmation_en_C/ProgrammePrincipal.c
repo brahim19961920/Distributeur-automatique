143db8058215a4762c42b6aae6f4c587032dea6b
brahim <brahimbenamiraca@gmail.com>
2021-02-24 00:44:48 +0100

    définition des structures Compartiment et Friandise

--- Programmation_en_C/ProgrammePrincipal.c
+++ Programmation_en_C/ProgrammePrincipal.c
@@ -8,15 +8,14 @@ struct Friandise{
 	int quantite;
 };
 
-//Fonction pour vérifier si un type de friandise est toujours disponible
-int estEnstock(struct Friandise f){
-if (f.quantite>0)
-	return 1;
-	return 0;}
 
-//Fonction pour mettre à jour le stock de friandises (dimunier le nombre de friandises
-void MiseAjourStock(struct Friandise f){
-	f.quantite-=1;}
+//Définition de la structure Compartiment 
+struct Compartiment{
+	//3 attributs
+	int numero; //numéro du compartiment
+	int valeur; // valeur de la piéce de monnaie
+	int nombre; // nombre de piéces de monnaie
+};
 
 
 
