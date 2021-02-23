f19bb6411fcc5d6b1a76ef73f3edd906adf7509e
brahim <brahimbenamiraca@gmail.com>
2021-02-24 00:45:46 +0100

    Ajout de quelques fonctions simples

--- Programmation_en_C/ProgrammePrincipal.c
+++ Programmation_en_C/ProgrammePrincipal.c
@@ -18,6 +18,37 @@ struct Compartiment{
 };
 
 
+//Fonction pour vérifier si un type de friandise est toujours disponible
+int estEnstock(struct Friandise f){
+if (f.quantite>0)
+	return 1;
+	return 0;}
+
+//Fonction pour mettre à jour le stock de friandises (dimunier le nombre de friandises
+void MiseAjourStock(struct Friandise f){
+	f.quantite-=1;}
+
+
+//Fonction pour vérifier si on peut rendre une piéce de monnaie à partir d'un compartiment
+int AutoriserRendrePiece(struct Compartiment c){
+        if(c.nombre>0)
+		return 1;
+		return 0;}
+
+//Fonction pour vérifier si on peut ajouter une piéce de monnaie à un compartiment
+int AutoriserAjouterPiece(struct Compartiment c){
+        if(c.nombre<50)
+		return 1;
+		return 0;}
+
+//Fonction pour ajouter une piéce à un compartiment 
+ void AjouterPiece(struct  Compartiment c){
+            c.nombre+=1;}
+
+//Méthode pour rendre une piéce de monnaie à partir d'un compartiment
+void RendrePiece(struct  Compartiment c){
+            c.nombre-=1;}
+
 
 int main(){
 return 0;
